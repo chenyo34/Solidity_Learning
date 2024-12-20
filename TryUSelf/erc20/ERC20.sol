d7cbcf18d016298f319efb45a33b0eb63ca1e055
chenyo34 <2277208307@qq.com>
2024-12-20 10:51:11 -0500

    Added and Completed erc20

---
+++ TryUSelf/erc20/ERC20.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import './IERC20.sol';
+
+contract ERC20 is IERC20 {
+    /*
+     *State Variables
+     */
+    mapping (address => uint256) public override balanceOf;
+
+    mapping (address => mapping (address => uint256)) public override allowance;
+
+    uint256 public override totalSupply;
+
+    string public tokenName;
+
+    string public symbol;
+
+    uint8 public decimals;
+
+    constructor(string memory _tokenName, string memory _symbol){
+        tokenName = _tokenName;
+        symbol = _symbol;
+    }
+    
+
+    function transfer(address _to_account, uint256 value) public override returns (bool){
+        // Assert the calling account has enough tokens to send value-worth token to _to account
+
+        if (balanceOf[msg.sender] < value){
+            return false;
+        }
+        // OR require(balanceOf[msg.sender] >= value, "Token is not enough to make this transfer!!!");
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[_to_account] += value;
+        emit Transfer(msg.sender, _to_account, value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 value) public override returns (bool){
+        
+        if (balanceOf[msg.sender] < value){
+            return false;
+        }
+        // OR require(balanceOf[msg.sender] >= value, "Token is not enough to make this transfer!!!");
+        allowance[msg.sender][_spender] = value;
+        emit Approval(msg.sender, _spender, value);
+        return true;
+
+    }
+
+    function transferFrom(address _from,
+                            address _to,
+                            uint256 value) public override returns (bool){
+
+    if ( (balanceOf[_from] >= value) && (allowance[_from][_to] >= value)){
+        allowance[_from][_to] -= value;
+        balanceOf[_from] -= value;
+        balanceOf[_to] += value;
+        emit Transfer(_from, _to, value);
+    } else {
+        return false;
+    }
+    
+    }
+
+    function mint(uint256 amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
+
+
+
+
+}
+
---
+++ TryUSelf/erc20/guidance.sol
@@ -0,0 +1,79 @@
+
+    // Create two files
+    // IERC20.sol defines the standard ERC20 interface.  
+    // ERC20.sol inherits and overrides IERC20, 
+        // adding two new functions: mint() and burn().  
+        // Both require an integer parameter and adjust totalSupply when tokens are created or destroyed.  
+
+
+
+    //The following is the basic description for the IERC20, try it yourself.
+
+    //==========
+    //IERC20.sol
+    //==========
+    
+    /*************
+    Events 
+    *************/
+
+    /**
+     * @dev Triggered when `value` tokens are transferred from `from` to `to`.
+     */
+
+
+    /**
+     * @dev Triggered whenever `value` tokens are approved by `owner` to be spent by `spender`.
+     */                
+
+
+    /*************
+    Functions
+    *************/
+
+    /**
+     * @dev Returns the total amount of tokens.
+     */
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+
+    /**
+     * @dev Transfers `amount` tokens from the caller's account to the recipient `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded or not.
+     *
+     * Emits a {Transfer} event.
+     */
+
+    /**
+     * @dev Allows `spender` to spend `amount` tokens from caller's account.
+     *
+     * Returns a boolean value indicating whether the operation succeeded or not.
+     *
+     * Emits an {Approval} event.
+     */
+
+    /**
+     * @dev Returns the amount authorized by the `owner` account to the `spender` account, default is 0.
+     *
+     * When {approve} or {transferFrom} is invoked，`allowance` will be changed.
+     */
+
+    /**
+     * @dev Transfer `amount` of tokens from `from` account to `to` account, subject to the caller's
+     * allowance. The caller must have allowance for `from` account balance.
+     *
+     * Returns `true` if the operation is successful.
+     *
+     * Emits a {Transfer} event.
+     */
+
+    //==========
+    //ERC20.sol
+    //==========
+    
+    // This implementation primarily overrides the IREC20 interface, thus the structure has been generally determined.
+    // However, the ERC20 implementation remains flexible, leaving room for custom functionality.
+    // Consider adding enhancements to improve functionality, usability, and compatibility where appropriate.
