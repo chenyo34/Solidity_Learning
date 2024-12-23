1fada128b379e7d92c3c55fbff1f9c3d4365afa0
chenyo34 <2277208307@qq.com>
2024-12-22 21:00:36 -0500

    Update all

---
+++ TryUSelf/ERC721/IERC721.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import './IERC165.sol';
+
+interface IERC721 is IERC164{
+    
+    /* Events */
+    event Transfer(address indexed from,
+                    address indexed to,
+                    uint256 indexed tokenId);
+    event Approval(address indexed owner,
+                    address indexed approved_address,
+                    uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner,
+                        address indexed operator,
+                        bool approved);
+
+    /* functions */
+    function balanceOf(address account) external view returns (uint256);
+    function ownerOf(uint256 tokenId) external view returns (address);
+    function transferFrom(address from,
+                            address to,
+                            uint256 tokenId) external;
+    function safeTransferFrom(address from,
+                                address to,
+                                uint256 tokenId) external ;
+    function approve(address approve_add, uint256 tokenId) external;
+    function getApproved(uint256 tokenId) external view returns (address);
+    function setApprovalForAll(address operator, bool _approved) external returns (bool);
+    function isApprovedForAll(address account, address operator) external view returns (bool);
+    function safeTransferFrom(address from,
+                                address to,
+                                uint256 tokenId,
+                                bytes calldata data) external ;
+}
